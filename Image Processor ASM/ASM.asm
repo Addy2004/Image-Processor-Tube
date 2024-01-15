f3535f4866f5208c64e6dd9b8dffddff5a06ad19
Addy2004 <40354792+Addy2004@users.noreply.github.com>
2024-01-15 16:46:56 +0530

    silly bugfix for ASM subtract

--- Image Processor ASM/ASM.asm
+++ Image Processor ASM/ASM.asm
@@ -31,10 +31,10 @@ SubtractBrightness:
 	neg r8w                     ; Negate the value to subtract
 
 MainLoopSubtract:
-	mov al, byte ptr [rdx + 10] ; Read the next byte from original
+	mov al, byte ptr [rdx + r10] ; Read the next byte from original
 	sub al, r8b                 ; Subtract the Brightness
 	cmovc ax, r11w              ; Set to 255 on overflow
-	mov byte ptr [rcx + 10], al ; Store the answer in Scan0
+	mov byte ptr [rcx + r10], al ; Store the answer in Scan0
 
 	inc r10                     ; Move to next byte in arrays
 	dec r9d                     ; Decrement our counter
